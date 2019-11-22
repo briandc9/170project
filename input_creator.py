220ccc838494d937cb4fdd361472f2a8fd4fe14e
briandc9 <44731568+briandc9@users.noreply.github.com>
2019-11-21 17:57:56 -0800

    woiejf;oawefo
    update input output phase1

--- input_creator.py
+++ input_creator.py
@@ -1,26 +1,93 @@
-# Create new locations for Phase 1
-import numpy
-import random
-import scipy
+import networkx as nx
+import numpy as np
+import utils as utils
+import sys
+import matplotlib.pyplot as plt
 
-locations = 50
-homes = 20
-grid_multiplier = 2 # size of grid related to number of locations
-one_ways = 20
+def inputmaker(n):
+    if n == 50:
+        f = open("50.in", "w")
+    elif n == 100:
+        f = open("100.in", "w")
+    elif n == 200:
+        f = open("200.in", "w")
+    f.write(str(n))
+    f.write("\n")
+    f.write(str(int(n / 2)))
+    f.write("\n")
+    f.write(str(0))
+    for i in np.arange(1, n):
+        f.write(" ")
+        f.write(str(i))
+    f.write("\n")
+    f.write(str(0))
+    for i in np.arange(2, n, 2):
+        f.write(" ")
+        f.write(str(i))
+    f.write("\n")
+    f.write(str(0))
+    f.write("\n")
+    for i in np.arange(n):
+        for j in np.arange(n):
+            f.write(str(adjlist[i][j]))
+            if not j == n - 1:
+                f.write(" ")
+        if not i == n - 1:
+            f.write("\n")
+    f.close()
+    
+def outputmaker(n):
+    if n == 50:
+        f = open("50.out", "w")
+    elif n == 100:
+        f = open("100.out", "w")
+    elif n == 200:
+        f = open("200.out", "w")
+    f.write(str(0))
+    f.write("\n")
+    f.write(str(1))
+    f.write("\n")
+    f.write(str(0))
+    for i in np.arange(2, n, 2):
+        f.write(" ")
+        f.write(str(i))
+    f.write(" ")
+    f.write(str(0))
+    f.close()
 
-# 50.in - at most 50 locs, at most 25 TAs
-# 100.in - <= 100 locs, <= 50 TAs
-# 200.in - <= 200 locs, <= 100 TAs 
-# make x random homes one way in/out
+def set_weights(G):
+    for u, v in G.edges:
+        G[u][v]['cost'] = np.random.randint(1, 5000)
+    
+def set_AM(G):
+    init = [['x' for i in np.arange(n)] for j in np.arange(n)]
+    for u, v in G.edges:
+        if shortest_lengths[u][v] < G[u][v]['cost']:
+            G[u][v]['cost'] = shortest_lengths[u][v]
+        init[u][v] = G[u][v]['cost']
+        init[v][u] = G[v][u]['cost']
+    for i in np.arange(n):
+        for j in np.arange(n):
+            if i == j or init[i][j] == 0:
+                init[i][j] = 'x'
+    return init
 
-def create_graph(num_locs = locations, num_hom = homes, mult = grid_multiplier):
-	#0 if just a node, 1 if a home
-	locs = numpy.ones(num_locs)
-	locs[:num_hom] = [("home_" + str(i)) for i in range(num_hom)]
-	random.shuffle(locs)
+# n = 50
+# n = 100
+n = int(input("n: "))
+# interchange probabilities
+if n == 50:
+    G = nx.gnp_random_graph(n, 0.5)
+elif n == 100:
+    G = nx.gnp_random_graph(n, 0.5)
+elif n == 200:
+    G = nx.gnp_random_graph(n, 0.5)
+    
+set_weights(G)
+shortest_lengths = dict(nx.floyd_warshall(G, weight='cost'))
 
-	print(locs)
+adjlist = set_AM(G)
+adjlist = np.reshape(adjlist, (n, n))
 
-
-
-create_graph()
+inputmaker(n)
+outputmaker(n)
